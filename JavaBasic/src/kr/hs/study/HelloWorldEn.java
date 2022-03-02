35b2cbd882e2c575257787520adb096093d92f45
m04j00 <m04j00@gmail.com>
2022-03-02 11:48:15 +0900

    0302. java 세팅

---
+++ JavaBasic/src/kr/hs/study/HelloWorldEn.java
@@ -0,0 +1,14 @@
+package kr.hs.study;
+
+public class HelloWorldEn {
+
+	private static void SayHello() {
+		System.out.println("hello");
+	}
+	
+	public static void main(String[] args) {
+		SayHello();
+
+	}
+
+}
