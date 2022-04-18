c6f39b53221624ac116abfba213f5c9711b3647b
m04j00 <m04j00@gmail.com>
2022-04-18 11:20:07 +0900

    AOP의 around, afterReturning, afterThrowing method 실습

--- spring-core/AOP/src/main/java/kr/hs/study/advisor/advisorClass.java
+++ spring-core/AOP/src/main/java/kr/hs/study/advisor/advisorClass.java
@@ -1,10 +1,24 @@
 package kr.hs.study.advisor;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class advisorClass {
 	public void beforeMethod() {
 		System.out.println("advice의 before method");
 	}
 	public void afterMethod() {
 		System.out.println("advice의 after method");
+	}
+	public void arroundMethod(ProceedingJoinPoint pjp) throws Throwable{
+		System.out.println("arround Method 호출 1");
+		pjp.proceed(); // 핵심 메소드 실행
+		System.out.println("arround Method 호출 2");
+	}
+	public void afterReturningMethod() {
+		System.out.println("afterReturningMethod 호출");
+	}
+	public void afterThrowingMethod(Throwable e) {
+		System.out.println("afterThrowing Method 호출");
+		System.out.println("e : " + e);
 	}
 }
